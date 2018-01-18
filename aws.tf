cdd0e98a783cd0284d7c1224465525436e2a1640
Sergey Titov <sergtitov@gmail.com>
2018-01-18 14:47:57 -0800

    Create aws.tf

---
+++ aws.tf
@@ -0,0 +1,16 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {
+  default = "us-east-1"
+}
+
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
